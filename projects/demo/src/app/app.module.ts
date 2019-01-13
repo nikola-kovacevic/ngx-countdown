b7fbf22e42c7ca127393295afb1807123592bf54
Nikola Kovacevic <nikolakovacevic.zg@gmail.com>
2019-01-13 10:50:33 +0100

    feat(publish): prepare package.json for publishing, tested installation and functionality of package from tarball, included polyfills for IE in demo

--- projects/demo/src/app/app.module.ts
+++ projects/demo/src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CountdownModule } from '@nkovacevic/ngx-countdown';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CountdownModule
   ],
   providers: [],
   bootstrap: [AppComponent]
