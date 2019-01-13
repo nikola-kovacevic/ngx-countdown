0ec1c9ff5359137e1fb68b03d321462e13602368
Nikola Kovacevic <nikolakovacevic.zg@gmail.com>
2019-01-13 09:33:58 +0100

    Initial commit

---
+++ projects/countdown/src/lib/countdown.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ngx-countdown',
+  template: `
+    <p>
+      countdown works!
+    </p>
+  `,
+  styles: []
+})
+export class CountdownComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ projects/countdown/src/lib/countdown.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CountdownComponent } from './countdown.component';
+
+@NgModule({
+  declarations: [CountdownComponent],
+  imports: [
+  ],
+  exports: [CountdownComponent]
+})
+export class CountdownModule { }
